7f0c234be5b094797784213e4f44ef074141f142
Stanecki <daniel.stanecki@man.eu>
2019-07-08 22:26:31 +0200

    DB created

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-// index.js
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const product = require('./routes/product.route'); // Imports routes for the products
-
-
-// initialize our express app
 const app = express();
-app.use('/products', product);
 
+// Set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb+srv://someuser:abcd1234@productsdb-6tiig.mongodb.net/test?retryWrites=true&w=majority';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/products', product);
 
 let port = 1234;
 
 app.listen(port, () => {
     console.log('Server is up and running on port numner ' + port);
 });
-
--- routes/product.route.js
+++ routes/product.route.js
@@ -6,6 +6,10 @@ const product_controller = require('../controllers/product.controller');
 
 
 // a simple test url to check that all of our files are communicating correctly.
-router.get('/test', product_controller.test);
+
+router.post('/create', product_controller.product_create);
+
+
+
 
 module.exports = router;
