ba730bf9de2fb2e0ab17bce2f78964e45165c1f6
Stanecki <daniel.stanecki@man.eu>
2019-07-08 21:01:31 +0200

    created serwer port 1234

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// initialize our express app
+const app = express();
+
+let port = 1234;
+
+app.listen(port, () => {
+    console.log('Server is up and running on port numner ' + port);
+});
