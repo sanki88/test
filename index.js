58c90348d489ec26ff538aac828409b321645785
Stanecki <daniel.stanecki@man.eu>
2019-07-08 21:18:33 +0200

    MVC

--- index.js
+++ index.js
@@ -3,11 +3,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const product = require('./routes/product.route'); // Imports routes for the products
+
+
 // initialize our express app
 const app = express();
+app.use('/products', product);
+
 
 let port = 1234;
 
 app.listen(port, () => {
     console.log('Server is up and running on port numner ' + port);
 });
+
